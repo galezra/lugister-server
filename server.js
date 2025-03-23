0f42be2d3b6f8bd3842d1e3b0f9b57f0c44d8f14
Gal Ezra <gal.ezra@lusha.com>
2025-03-23 17:33:14 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server, {
+  cors: {
+    origin: "*",  // WARNING: In production, replace with your actual domain
+    methods: ["GET", "POST"]
+  }
+});
+var port = process.env.PORT || 3000;
+var numUsers = 0;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static('public'));
+
+
+app.get('/', async (req, res) => {
+  res.json({ hello: 'world', numUsers: numUsers });
+});
+
+
+io.on('connection', function (socket) {
+  numUsers++;
+
+  socket.on('sendEmojis', function () {
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+      --numUsers;
+  });
+});
